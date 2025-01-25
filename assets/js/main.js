d8ab61d1948fa674ef55ffb4838bf1a384a3cb11
HebaAlajouri <hebaajoury1212@gmail.com>
2025-01-25 20:19:56 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,38 +13,38 @@ tabs.forEach(tab => {
     tab.addEventListener("click", () => {
         const target = document.querySelector(tab.dataset.target);
 
-        // Remove 'skills__active' from all tab content
+        
         tabContent.forEach(tabContents => {
             tabContents.classList.remove("skills__active");
         });
 
-        // Add 'skills__active' to the selected tab content
+      
         target.classList.add('skills__active');
 
-        // Remove 'skills__active' from all tabs
+        
         tabs.forEach(tab => {
             tab.classList.remove("skills__active");
         });
 
-        // Add 'skills__active' to the selected tab
+       
         tab.classList.add('skills__active');
     });
 });
 // work
 const popup = document.getElementById('portfolioPopup');
-const openButtons = document.querySelectorAll('.work__button'); // Buttons to open the popup
-const closeButton = document.getElementById('closePopup'); // Close button
+const openButtons = document.querySelectorAll('.work__button');
+const closeButton = document.getElementById('closePopup'); 
 
 // Open popup
 openButtons.forEach(button => {
     button.addEventListener('click', () => {
-        popup.classList.add('active'); // Add active class to show the popup
+        popup.classList.add('active'); 
     });
 });
 
 // Close popup
 closeButton.addEventListener('click', () => {
-    popup.classList.remove('active'); // Remove active class to hide the popup
+    popup.classList.remove('active'); 
 });
 
 
